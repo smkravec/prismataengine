bd08e57ec5e943d382f676e8499ad5f1a59ecd6a
Nova Devereaux <nova@noblejury.com>
2020-11-20 08:16:30 -0800

    Support PRISMATA_INIT_CARD_PATH variable

--- prismataengine/__init__.py
+++ prismataengine/__init__.py
@@ -1,4 +1,9 @@
+from os import environ
 from _prismataengine import *
 from importlib import resources
-with resources.path(__name__, 'cardLibrary.jso') as path:
-    init(str(path))
+if "PRISMATA_INIT_CARD_PATH" in os.environ:
+    if os.environ.getenv("PRISMATA_INIT_CARD_PATH", None):
+        init(os.environ.getenv("PRISMATA_INIT_CARD_PATH", None))
+else:
+    with resources.path(__name__, 'cardLibrary.jso') as path:
+        init(str(path))
