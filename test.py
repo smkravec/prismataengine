543cd667a777727275aabd21160c115079395eae
Nova Devereaux <nova@noblejury.com>
2020-11-22 10:41:55 -0800

    Test

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import json, prismataengine as p, random
+from pprint import pprint
 import numpy
 import timeit, time
+
+class PythonRandomPlayer(object):
+    def getAction(self, gamestate):
+        state = gamestate.toVector()
+        actions = gamestate.getAbstractActions()
+        # print(actions)
+        # print([action.json() for action in gamestate._actions])
+        # print([str(p.ConcreteAction(gamestate, action)) for action in gamestate._actions])
+        action = random.choice(actions)
+        return action
 
 def runGame():
     gamestate = p.GameState('''{
@@ -15,25 +26,33 @@ def runGame():
                  {"cardName":"Engineer", "color":1, "amount":2}
              ],
              "cards":["Drone","Engineer","Blastforge","Steelsplitter"]
-         }''')
-    print('----------------NEW GAME NEW LIFE-------------')
+         }''')#, player1=PythonRandomPlayer(), player2=p.AIPlayerRandom(1))
+    # print('----------------NEW GAME NEW LIFE-------------')
     lastPlayer = 0
     while not gamestate.isGameOver():
         # print([(card.type, card.name) for card in gamestate.getLiveCards(gamestate.activePlayer)])
         state = gamestate.toVector()
+        print(gamestate.annotate(state))
         if gamestate.activePlayer != lastPlayer:
             lastPlayer = gamestate.activePlayer
-            print(gamestate.annotate(state))
-            print(numpy.array_str(state, max_line_width=120))
+            # pprint(gamestate.annotate(state))
+            # print(numpy.array_str(state, max_line_width=120))
             # print(gamestate.json())
-            time.sleep(0.1)
+            # time.sleep(0.1)
+        gamestate.step()
         actions = gamestate.getAbstractActions()
+        # print(actions)
+        # print([action.json() for action in gamestate._actions])
+        # print([str(p.ConcreteAction(gamestate, action)) for action in gamestate._actions])
         action = random.choice(actions)
         gamestate.doAction(action)
-    # print(numpy.array_str(state, max_line_width=120))
+    print(f"Winner: {gamestate.winner()}")
+    state = gamestate.toVector()
+    pprint(gamestate.annotate(state))
+    print(numpy.array_str(state, max_line_width=120))
 
-n = 100
-for n in range(n):
-    runGame()
-# time = timeit.timeit(runGame, setup='gc.enable()', number=n)
-# print(f"n = {n}, wall time = {time} ({1/time*100} Hz)")
+n = 1
+# for n in range(n):
+#     runGame()
+time = timeit.timeit(runGame, setup='gc.enable()', number=n)
+print(f"n = {n}, wall time = {time} ({1/time*n} Hz)")
